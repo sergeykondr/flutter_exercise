0d81d572f3fe29b897fe33a71cc8a0aa8109d6f9
sergeykondr <sergeykondr@gmail.com>
2020-05-02 00:40:54 +0300

    add 3 columns - test

--- hello_flutter/hello_flutter/lib/main.dart
+++ hello_flutter/hello_flutter/lib/main.dart
@@ -1,11 +1,61 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Center(
-      child: Text(
-    'Hello flutter! My first!',
-    textDirection: TextDirection.ltr,
-    style:
-        TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.red),
-  )));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        appBar: AppBar(
+          title: Text('My First App'),
+          centerTitle: true,
+        ),
+        body: Row(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Center(
+                  child: Text(
+                    'My 1',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Center(
+                  child: Text(
+                    'My 2',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Center(
+                  child: Text(
+                    'My 3',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
 }
