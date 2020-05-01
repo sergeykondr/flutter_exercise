e83df5e9923fc2f5bdc3aa8b27bd5da0d6f664f4
sergeykondr <sergeykondr@gmail.com>
2020-05-01 22:59:02 +0300

    fix

--- hello_flutter/hello_flutter/lib/main.dart
+++ hello_flutter/hello_flutter/lib/main.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Center
-  (
+  runApp(Center(
       child: Text(
     'Hello flutter! My first!',
     textDirection: TextDirection.ltr,
-    style: TextStyle(
-      fontSize: 30,
-      fontWeight: FontWeight.bold,
-      color: Colors.red
-    ),
+    style:
+        TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.red),
   )));
 }
