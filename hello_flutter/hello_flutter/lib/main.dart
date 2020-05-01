c53bd2f34d84afb37aa53a8ec4356b0f54e0dae3
sergeykondr <sergeykondr@gmail.com>
2020-05-01 22:40:31 +0300

    text color red and bold

---
+++ hello_flutter/hello_flutter/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Center(
+      child: Text(
+    'Hello flutter! My first!',
+    textDirection: TextDirection.ltr,
+    style: TextStyle(
+      fontSize: 30,
+      fontWeight: FontWeight.bold,
+      color: Colors.red
+    ),
+  )));
+}
