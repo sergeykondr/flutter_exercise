9e519a1d299a9f0c13de70da64c6df6d7e49b3b7
sergeykondr <sergeykondr@gmail.com>
2020-05-03 20:55:36 +0300

    add custom Font

--- hello_flutter/assets_example/lib/main.dart
+++ hello_flutter/assets_example/lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'ConcertOne',
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Adding Assets'),
@@ -20,6 +23,18 @@ class MyApp extends StatelessWidget {
                 image: AssetImage('assets/images/bg.jpg'),
               ),
               Image.asset('assets/icons/icon.png'),
+              Positioned(
+                top: 570,
+                left: 110,
+                child: Text(
+                  'My custom font',
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 42,
+                    //fontFamily: 'ConcertOne'
+                  ),
+                ),
+              ),
             ],
           ),
         ),
