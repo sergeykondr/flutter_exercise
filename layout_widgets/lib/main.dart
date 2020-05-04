cac8d68a0d22bdaa1b9084e806cd395bb95017fa
sergeykondr <sergeykondr@gmail.com>
2020-05-05 01:03:18 +0300

    Stack, Positioned

--- layout_widgets/lib/main.dart
+++ layout_widgets/lib/main.dart
@@ -11,33 +11,18 @@ class MyApp extends StatelessWidget {
           title: Text("Компоновка виджетов"),
           centerTitle: true,
         ),
-        body: Row(
+        body: Stack(
+          //alignment: Alignment.center,
           children: <Widget>[
-            Expanded(
-              flex: 4,
-              child: Image.network('https://avatars.mds.yandex.net/get-pdb/2884572/a0387273-0bea-4d21-a7ca-18b6c22ea727/s1200?webp=false')
-            ),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(25),
-                color: Colors.lightBlueAccent,
-                child: Text('1'),
+            Icon(Icons.airplay, size: 300, color: Colors.purple),
+            Positioned(
+              top: 65,
+              left: 50,
+              child: Text(
+                'TV',
+                style: TextStyle(fontSize: 50),
               ),
-            ),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(25),
-                color: Colors.yellowAccent,
-                child: Text('2'),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(25),
-                color: Colors.redAccent,
-                child: Text('3'),
-              ),
-            ),
+            )
           ],
         ),
       ),
