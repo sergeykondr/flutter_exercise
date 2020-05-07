09878252a978152f329211e83794058d283bcf38
sergeykondr <sergeykondr@gmail.com>
2020-05-07 18:00:50 +0300

    ListView Widget

--- list_view_example/lib/main.dart
+++ list_view_example/lib/main.dart
@@ -3,16 +3,54 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      
       home: Scaffold(
-
+        appBar: AppBar(
+          title: Text('ListView Widget'),
+          centerTitle: true,
+        ),
+        body: BodyListView(),
       ),
     );
   }
 }
 
+class BodyListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _myListView();
+  }
+}
+
+Widget _myListView() {
+  return ListView(
+    //shrinkWrap: true, //без нее заполняет по всей вертикале или горизонтале
+    //scrollDirection: Axis.horizontal,
+    //itemExtent: 300, //фикс значение (актуально при горизонтальном списке) ускоряет производительность flutter
+    //reverse: true,
+    padding: EdgeInsets.all(9.0),
+    children: <Widget>[
+      ListTile(
+        title: Text('Sun'),
+        subtitle: Text('Today Clear'),
+        leading: Icon(Icons.wb_sunny),
+        trailing: Icon(Icons.keyboard_arrow_right),
+      ),
+      ListTile(
+        title: Text('Cloudy'),
+        subtitle: Text('Today Cloudy'),
+        leading: Icon(Icons.wb_cloudy),
+        trailing: Icon(Icons.keyboard_arrow_right),
+      ),
+      ListTile(
+        title: Text('Snow'),
+        subtitle: Text('Today Snow'),
+        leading: Icon(Icons.ac_unit),
+        trailing: Icon(Icons.keyboard_arrow_right),
+      )
+    ],
+  );
+}
