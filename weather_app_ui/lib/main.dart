36bc6629e70b3010f59decccd762257048d93161
sergeykondr <sergeykondr@gmail.com>
2020-05-09 00:36:18 +0300

    some fix. Widget _week

--- weather_app_ui/lib/main.dart
+++ weather_app_ui/lib/main.dart
@@ -24,7 +24,6 @@ class MyApp extends StatelessWidget {
 }
 
 Widget _buildBody() {
-  double widthWeek = 150;
   return Container(
     padding: EdgeInsets.all(10),
     color: Colors.red,
@@ -161,269 +160,57 @@ Widget _buildBody() {
           child: ListView(
             scrollDirection: Axis.horizontal,
             children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(right: 10),
-                child: Container(
-                  width: widthWeek,
-                  color: Colors.red[200],
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text('Friday',
-                          style: TextStyle(fontSize: 24, color: Colors.white)),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 8),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Container(
-                              child: Text(
-                                '6 °F',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 28),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 10),
-                              child: Icon(
-                                Icons.wb_sunny,
-                                color: Colors.white,
-                                size: 40,
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 10),
-                child: Container(
-                  width: widthWeek,
-                  color: Colors.red[200],
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text('Saturday',
-                          style: TextStyle(fontSize: 24, color: Colors.white)),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 8),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Container(
-                              child: Text(
-                                '5 °F',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 28),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 10),
-                              child: Icon(
-                                Icons.wb_sunny,
-                                color: Colors.white,
-                                size: 40,
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 10),
-                child: Container(
-                  width: widthWeek,
-                  color: Colors.red[200],
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text('Sunday',
-                          style: TextStyle(fontSize: 24, color: Colors.white)),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 8),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Container(
-                              child: Text(
-                                '22 °F',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 28),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 10),
-                              child: Icon(
-                                Icons.wb_sunny,
-                                color: Colors.white,
-                                size: 40,
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 10),
-                child: Container(
-                  width: widthWeek,
-                  color: Colors.red[200],
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text('Monday',
-                          style: TextStyle(fontSize: 24, color: Colors.white)),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 8),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Container(
-                              child: Text(
-                                '6 °F',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 28),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 10),
-                              child: Icon(
-                                Icons.wb_sunny,
-                                color: Colors.white,
-                                size: 40,
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 10),
-                child: Container(
-                  width: widthWeek,
-                  color: Colors.red[200],
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text('Thuesday',
-                          style: TextStyle(fontSize: 24, color: Colors.white)),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 8),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Container(
-                              child: Text(
-                                '5 °F',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 28),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 10),
-                              child: Icon(
-                                Icons.wb_sunny,
-                                color: Colors.white,
-                                size: 40,
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 10),
-                child: Container(
-                  width: widthWeek,
-                  color: Colors.red[200],
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text('Wednesday',
-                          style: TextStyle(fontSize: 24, color: Colors.white)),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 8),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Container(
-                              child: Text(
-                                '6 °F',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 28),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 10),
-                              child: Icon(
-                                Icons.wb_sunny,
-                                color: Colors.white,
-                                size: 40,
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 10),
-                child: Container(
-                  width: widthWeek,
-                  color: Colors.red[200],
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text('Thursday',
-                          style: TextStyle(fontSize: 24, color: Colors.white)),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 8),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Container(
-                              child: Text(
-                                '6 °F',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 28),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 10),
-                              child: Icon(
-                                Icons.wb_sunny,
-                                color: Colors.white,
-                                size: 40,
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
+              _week('Friday', 6),
+              _week('Saturday', 5),
+              _week('Sunday', 22),
+              _week('Monday', 18),
+              _week('Thuesday', 20),
+              _week('Wednesday', 23),
+              _week('Thursday', 19)
             ],
           ),
         ),
       ],
+    ),
+  );
+}
+
+Widget _week(String day, int temperature) {
+  double widthWeek = 150;
+
+  return Padding(
+    padding: const EdgeInsets.only(right: 10),
+    child: Container(
+      width: widthWeek,
+      color: Colors.red[200],
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(day, style: TextStyle(fontSize: 24, color: Colors.white)),
+          Padding(
+            padding: const EdgeInsets.only(top: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    '$temperature °F',
+                    style: TextStyle(color: Colors.white, fontSize: 28),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Icon(
+                    Icons.wb_sunny,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     ),
   );
 }
