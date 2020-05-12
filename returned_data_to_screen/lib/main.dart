7f3e8203b171b17450a27ddfb449954c55d2093f
sergeykondr <sergeykondr@gmail.com>
2020-05-12 22:50:12 +0300

    returned data to screen

---
+++ returned_data_to_screen/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:returned_data_to_screen/first_screen.dart';
+
+void main() => runApp(
+  MaterialApp(
+    title: 'Returned data',
+    home: FirstScreen(),
+  )
+);
