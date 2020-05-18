65e92c370c5641049ed71e8ecad7710114e3147a
sergeykondr <sergeykondr@gmail.com>
2020-05-18 14:51:41 +0300

    Provider, ColorChangeNotifier. homework

---
+++ homework_provider/android/app/src/main/kotlin/com/example/homework_provider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.homework_provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ homework_provider/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'dart:math' as math;
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ColorChangeNotifier(),
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    ColorChangeNotifier _state = Provider.of<ColorChangeNotifier>(context);
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Provider - Homework',
+            style: TextStyle(color: _state._color),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.black,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              AnimatedContainer(
+                duration: Duration(milliseconds: 400),
+                color: _state._color,
+                width: 200,
+                height: 200,
+              ),
+              Switch(
+                value: _state._valueCheked,
+                onChanged: (isCheked) {
+                  _state.rndColor(isCheked);
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ColorChangeNotifier extends ChangeNotifier {
+  bool _valueCheked = false;
+  Color _color = Colors.green;
+
+  void rndColor(isChaked) {
+    _valueCheked = isChaked;
+    _color = Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+    notifyListeners();
+  }
+}
